3b363d6fa59898b125bbde893e6b817dd305a4f9
Alyssa Biasi <biasi91@gmail.com>
2015-11-11 08:47:04 +1100

    Init the store and start the vote on start up

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import startServer from './server';
 export const store = makeStore();
 startServer(store);
 
+const entries = require('./data/entries.json');
+store.dispatch({ type: 'SET_ENTRIES', entries });
+store.dispatch({ type: 'NEXT_VOTE' });
+
